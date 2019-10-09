1fd5a189c009a8afc7e57c77858da046a8df73b4
BeyerJB <MasterChef325@yahoo.com>
2019-10-09 15:07:34 -0400

    Secondary upload test

--- src/com/game/backend/moveCheck.java
+++ src/com/game/backend/moveCheck.java
@@ -2,9 +2,9 @@ package com.game.backend;
 
 public class moveCheck {
 
-	public static boolean isMoveValid(room[] Room, player[] player, int desiredRoom) {
+	//public static boolean isMoveValid(room[] Room, player[] player, int desiredRoom) {
 		//aauihiafjofjafl
 
-	}
+	//}
 
 }
